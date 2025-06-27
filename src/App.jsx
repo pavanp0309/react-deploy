d2a30d42e449fb20c400ffaed300cadcdd0a4293
pavanp0309 <pavanreddyp0309@gmail.com>
2025-06-27 15:39:37 +0530

    completed deployement

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+let api_key =import.meta.env.VITE_API_KEY
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello welcome</h1>
+    </div>
+  )
+}
+
+export default App
